15e7f8d4639ed4c5cb5dee74c64dd5216d41351d
SeiBrynn <bryancuellar266@gmail.com>
2024-06-13 17:05:30 +0400

    Add files via upload

--- Inventory.java
+++ Inventory.java
@@ -19,10 +19,10 @@ import java.awt.event.ActionEvent;
 public class Inventory {
 
     JFrame Inventory;
-    private JTable table;
+    private JTable tblList;
     private DefaultTableModel tableModel;
-    private JTextField searchBar;
-    private JTextField addCategoryField, addProductField;
+    private JTextField txtSearchBar;
+    private JTextField txtCategory, txtProduct;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -47,6 +47,11 @@ public class Inventory {
         Inventory.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Inventory.setExtendedState(JFrame.MAXIMIZED_BOTH);
         Inventory.getContentPane().setLayout(null);
+
+        JPanel pnlHeader = new JPanel();
+        pnlHeader.setBackground(new Color(128, 128, 128));
+        pnlHeader.setBounds(274, 0, 1650, 90);
+        Inventory.getContentPane().add(pnlHeader);
 
         JPanel panel = new JPanel();
         panel.setBackground(new Color(192, 192, 192));
@@ -115,89 +120,154 @@ public class Inventory {
         lblAccount.setFont(new Font("Tahoma", Font.PLAIN, 24));
         lblAccount.setBounds(20, 345, 245, 40);
         panel.add(lblAccount);
-        
+
         JPanel contentPanel = new JPanel();
-        contentPanel.setBounds(280, 0, 1660, 1061);
+        contentPanel.setBounds(277, 4, 1647, 700);
         Inventory.getContentPane().add(contentPanel);
         contentPanel.setLayout(null);
 
-        JPanel topPanel = new JPanel();
-        topPanel.setBounds(0, 0, 1660, 30);
-        contentPanel.add(topPanel);
-        topPanel.setLayout(null);
-        
-        searchBar = new JTextField();
-        searchBar.setBounds(60, 5, 200, 20);
-        topPanel.add(new JLabel("Search:")).setBounds(10, 5, 50, 20);
-        topPanel.add(searchBar);
+        JPanel pnlTop = new JPanel();
+        pnlTop.setBounds(10, 125, 1660, 30);
+        contentPanel.add(pnlTop);
+        pnlTop.setLayout(null);
+
+        txtSearchBar = new JTextField();
+        txtSearchBar.setBounds(85, 11, 200, 20);
+        pnlTop.add(txtSearchBar);
+
+        JLabel lblSearch = new JLabel("Search:");
+        lblSearch.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblSearch.setBounds(10, 12, 90, 14);
+        pnlTop.add(lblSearch);
 
         // Table
-        String[] columnNames = {"Category", "Product", "Price", "Stock"};
+        String[] columnNames = {"Category", "Product", "Price"};
         Object[][] initialData = {
-                {"NOVEL", "Crime and Punishment", 0.0, 0},
-                {"NOVEL", "The Songs of Achilles", 0.0, 0},
-                {"NOVEL", "Dune", 0.0, 0},
-                {"SELF-HELP/PHILOSOPHY", "Ego is the Enemy", 0.0, 0},
-                {"SELF-HELP/PHILOSOPHY", "Meditations", 0.0, 0},
-                {"SELF-HELP/PHILOSOPHY", "The Mountain is You", 0.0, 0},
-                {"HISTORY", "The Diary of a Girl", 0.0, 0},
-                {"HISTORY", "Noli me Tangere", 0.0, 0},
-                {"HISTORY", "The First Voyage", 0.0, 0}
+                {"NOVEL", "Dune", 0.0},
+                {"NOVEL", "Crime and Punishment", 0.0},
+                {"NOVEL", "The Song of Achilles", 0.0},
+                {"SELF-HELP/PHILOSOPHY", "Meditations", 0.0},
+                {"SELF-HELP/PHILOSOPHY", "Ego is the Enemy", 0.0},
+                {"SELF-HELP/PHILOSOPHY", "The Mountain is You", 0.0},
+                {"HISTORY", "The Diary of a Girl", 0.0},
+                {"HISTORY", "Noli me Tangere", 0.0},
+                {"HISTORY", "The First Voyage", 0.0}
         };
 
         tableModel = new DefaultTableModel(initialData, columnNames) {
             public boolean isCellEditable(int row, int column) {
-                return column == 2 || column == 3;
+                return column == 2;
             }
 
             public Class<?> getColumnClass(int columnIndex) {
                 if (columnIndex == 2) return Double.class;
-                if (columnIndex == 3) return Integer.class;
                 return super.getColumnClass(columnIndex);
             }
         };
 
-        table = new JTable(tableModel);
-        JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(0, 30, 1660, 500);
+        tblList = new JTable(tableModel);
+        tblList.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        tblList.setRowHeight(30);
+        JScrollPane scrollPane = new JScrollPane(tblList);
+        scrollPane.setBounds(0, 166, 1300, 696);
         contentPanel.add(scrollPane);
+        
+        JLabel lblStock = new JLabel("Stock");
+        lblStock.setBounds(1449, 173, 49, 14);
+        contentPanel.add(lblStock);
+        
+        JLabel lblNovel1 = new JLabel("0");
+        lblNovel1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblNovel1.setBounds(1310, 191, 49, 25);
+        contentPanel.add(lblNovel1);
+        
+        JLabel lblNovel2 = new JLabel("0");
+        lblNovel2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblNovel2.setBounds(1310, 224, 49, 25);
+        contentPanel.add(lblNovel2);
+        
+        JLabel lblNovel3 = new JLabel("0");
+        lblNovel3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblNovel3.setBounds(1310, 252, 49, 25);
+        contentPanel.add(lblNovel3);
+        
+        JLabel lblPhilo1 = new JLabel("0");
+        lblPhilo1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblPhilo1.setBounds(1310, 282, 49, 25);
+        contentPanel.add(lblPhilo1);
+        
+        JLabel lblPhilo2 = new JLabel("0");
+        lblPhilo2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblPhilo2.setBounds(1310, 316, 49, 25);
+        contentPanel.add(lblPhilo2);
+        
+        JLabel lblPhilo3 = new JLabel("0");
+        lblPhilo3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblPhilo3.setBounds(1310, 347, 49, 25);
+        contentPanel.add(lblPhilo3);
+        
+        JLabel lblHis1 = new JLabel("0");
+        lblHis1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblHis1.setBounds(1310, 374, 49, 25);
+        contentPanel.add(lblHis1);
+        
+        JLabel lblHis2 = new JLabel("0");
+        lblHis2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblHis2.setBounds(1310, 405, 49, 25);
+        contentPanel.add(lblHis2);
+        
+        JLabel lblHis3 = new JLabel("0");
+        lblHis3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblHis3.setBounds(1310, 433, 49, 25);
+        contentPanel.add(lblHis3);
 
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setBounds(0, 530, 1660, 100);
-        contentPanel.add(bottomPanel);
-        bottomPanel.setLayout(null);
+        JPanel pnlBottom = new JPanel();
+        pnlBottom.setBounds(280, 711, 1660, 100);
+        Inventory.getContentPane().add(pnlBottom);
+        pnlBottom.setLayout(null);
 
-        addCategoryField = new JTextField();
-        addCategoryField.setBounds(100, 10, 150, 20);
-        bottomPanel.add(new JLabel("Category:")).setBounds(30, 10, 70, 20);
-        bottomPanel.add(addCategoryField);
+        txtCategory = new JTextField();
+        txtCategory.setBounds(101, 10, 150, 20);
+        pnlBottom.add(txtCategory);
 
-        addProductField = new JTextField();
-        addProductField.setBounds(400, 10, 150, 20);
-        bottomPanel.add(new JLabel("Product:")).setBounds(330, 10, 70, 20);
-        bottomPanel.add(addProductField);
+        txtProduct = new JTextField();
+        txtProduct.setBounds(400, 10, 150, 20);
+        pnlBottom.add(txtProduct);
 
-        JButton addButton = new JButton("Add Item");
-        addButton.setBounds(600, 10, 150, 30);
-        bottomPanel.add(addButton);
+        JButton btnAddItem = new JButton("Add Item");
+        btnAddItem.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        btnAddItem.setBounds(600, 10, 150, 30);
+        pnlBottom.add(btnAddItem);
 
-        JButton removeButton = new JButton("Remove Product");
-        removeButton.setBounds(800, 10, 150, 30);
-        bottomPanel.add(removeButton);
+        JButton btnRemoveProduct = new JButton("Remove Product");
+        btnRemoveProduct.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        btnRemoveProduct.setBounds(800, 10, 150, 30);
+        pnlBottom.add(btnRemoveProduct);
 
-        JButton requestRestockButton = new JButton("Request Re-stock");
-        requestRestockButton.setBounds(1000, 10, 150, 30);
-        bottomPanel.add(requestRestockButton);
+        JButton btnRestock = new JButton("Request Re-stock");
+        btnRestock.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        btnRestock.setBounds(1000, 10, 186, 30);
+        pnlBottom.add(btnRestock);
 
-        addButton.addActionListener(new ActionListener() {
+        JLabel lblProduct = new JLabel("Product:");
+        lblProduct.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblProduct.setBounds(324, 11, 114, 14);
+        pnlBottom.add(lblProduct);
+
+        JLabel lblCategory = new JLabel("Category:");
+        lblCategory.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        lblCategory.setBounds(23, 10, 78, 27);
+        pnlBottom.add(lblCategory);
+
+        btnAddItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	
+
             }
         });
 
-        removeButton.addActionListener(new ActionListener() {
+        btnRemoveProduct.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	
+
             }
         });
     }
